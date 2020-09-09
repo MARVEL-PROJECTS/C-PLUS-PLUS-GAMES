42b6cb936208d1b297b7c92615bd8abc076f79dd
Dreadful <70088733+akkiteja@users.noreply.github.com>
2020-09-09 09:32:43 +0530

    Games

--- main.cpp
+++ main.cpp
@@ -1,176 +1,176 @@
 #include <SFML/Graphics.hpp>
-#include <time.h>
-#include "Connector.hpp"
 using namespace sf;
 
-int size = 56;
-Vector2f offset(28,28);
+int width = 1024;
+int height = 768;
+int roadW = 2000;
+int segL = 200; //segment length
+float camD = 0.84; //camera depth
 
-Sprite f[32]; //figures
-std::string position="";
-
-int board[8][8] = 
-    {-1,-2,-3,-4,-5,-3,-2,-1,
-     -6,-6,-6,-6,-6,-6,-6,-6,
-      0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0,
-      6, 6, 6, 6, 6, 6, 6, 6,
-      1, 2, 3, 4, 5, 3, 2, 1};
-
-std::string toChessNote(Vector2f p)
+void drawQuad(RenderWindow &w, Color c, int x1,int y1,int w1,int x2,int y2,int w2)
 {
-  std::string s = "";
-  s += char(p.x/size+97);
-  s += char(7-p.y/size+49);
-  return s;
+    ConvexShape shape(4);
+    shape.setFillColor(c);
+    shape.setPoint(0, Vector2f(x1-w1,y1));
+    shape.setPoint(1, Vector2f(x2-w2,y2));
+    shape.setPoint(2, Vector2f(x2+w2,y2));
+    shape.setPoint(3, Vector2f(x1+w1,y1));
+    w.draw(shape);
 }
 
-Vector2f toCoord(char a,char b)
+struct Line
 {
-   int x = int(a) - 97;
-   int y = 7-int(b)+49;
-   return Vector2f(x*size,y*size);
-}
+  float x,y,z; //3d center of line
+  float X,Y,W; //screen coord
+  float curve,spriteX,clip,scale;
+  Sprite sprite;
 
-void move(std::string str)
-{
-    Vector2f oldPos = toCoord(str[0],str[1]);
-    Vector2f newPos = toCoord(str[2],str[3]);
+  Line()
+  {spriteX=curve=x=y=z=0;}
 
-    for(int i=0;i<32;i++)
-     if (f[i].getPosition()==newPos) f[i].setPosition(-100,-100);
-        
-    for(int i=0;i<32;i++)
-     if (f[i].getPosition()==oldPos) f[i].setPosition(newPos);
+  void project(int camX,int camY,int camZ)
+  {
+    scale = camD/(z-camZ);
+    X = (1 + scale*(x - camX)) * width/2;
+    Y = (1 - scale*(y - camY)) * height/2;
+    W = scale * roadW  * width/2;
+  }
 
-    //castling       //if the king didn't move
-    if (str=="e1g1") if (position.find("e1")==-1) move("h1f1"); 
-    if (str=="e8g8") if (position.find("e8")==-1) move("h8f8");
-    if (str=="e1c1") if (position.find("e1")==-1) move("a1d1");
-    if (str=="e8c8") if (position.find("e8")==-1) move("a8d8");
-}
+  void drawSprite(RenderWindow &app)
+  {
+    Sprite s = sprite;
+    int w = s.getTextureRect().width;
+    int h = s.getTextureRect().height;
 
-void loadPosition()
-{
-    int k=0;
-    for(int i=0;i<8;i++)
-    for(int j=0;j<8;j++)
-     {
-       int n = board[i][j];
-       if (!n) continue;
-       int x = abs(n)-1;
-       int y = n>0?1:0;
-       f[k].setTextureRect( IntRect(size*x,size*y,size,size) );
-       f[k].setPosition(size*j,size*i);
-       k++;
-     }
+    float destX = X + scale * spriteX * width/2;
+    float destY = Y + 4;
+    float destW  = w * W / 266;
+    float destH  = h * W / 266;
 
-    for(int i=0;i<position.length();i+=5)
-      move(position.substr(i,4));
-}
+    destX += destW * spriteX; //offsetX
+    destY += destH * (-1);    //offsetY
+
+    float clipH = destY+destH-clip;
+    if (clipH<0) clipH=0;
+
+    if (clipH>=destH) return;
+    s.setTextureRect(IntRect(0,0,w,h-h*clipH/destH));
+    s.setScale(destW/w,destH/h);
+    s.setPosition(destX, destY);
+    app.draw(s);
+    }
+};
 
 
 int main()
 {
-    RenderWindow window(VideoMode(504, 504), "The Chess! (press SPACE)");
+    RenderWindow app(VideoMode(width, height), "Outrun Racing!");
+    app.setFramerateLimit(60);
 
-    ConnectToEngine("stockfish.exe");    
+    Texture t[50];
+    Sprite object[50];
+    for(int i=1;i<=7;i++)
+     {
+       t[i].loadFromFile("images/"+std::to_string(i)+".png");
+       t[i].setSmooth(true);
+       object[i].setTexture(t[i]);
+     }
 
-    Texture t1,t2;
-    t1.loadFromFile("images/figures.png"); 
-    t2.loadFromFile("images/board.png");
+    Texture bg;
+    bg.loadFromFile("images/bg.png");
+    bg.setRepeated(true);
+    Sprite sBackground(bg);
+    sBackground.setTextureRect(IntRect(0,0,5000,411));
+    sBackground.setPosition(-2000,0);
 
-    for(int i=0;i<32;i++) f[i].setTexture(t1);
-    Sprite sBoard(t2); 
+    std::vector<Line> lines;
 
-    loadPosition();
+    for(int i=0;i<1600;i++)
+     {
+       Line line;
+       line.z = i*segL;
 
-    bool isMove=false;
-    float dx=0, dy=0;
-    Vector2f oldPos,newPos;
-    std::string str;
-    int n=0; 
+       if (i>300 && i<700) line.curve=0.5;
+       if (i>1100) line.curve=-0.7;
 
-    while (window.isOpen())
+       if (i<300 && i%20==0) {line.spriteX=-2.5; line.sprite=object[5];}
+       if (i%17==0)          {line.spriteX=2.0; line.sprite=object[6];}
+       if (i>300 && i%20==0) {line.spriteX=-0.7; line.sprite=object[4];}
+       if (i>800 && i%20==0) {line.spriteX=-1.2; line.sprite=object[1];}
+       if (i==400)           {line.spriteX=-1.2; line.sprite=object[7];}
+
+       if (i>750) line.y = sin(i/30.0)*1500;
+
+       lines.push_back(line);
+     }
+
+   int N = lines.size();
+   float playerX = 0;
+   int pos = 0;
+   int H = 1500;
+
+    while (app.isOpen())
     {
-        Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
-
         Event e;
-        while (window.pollEvent(e))
+        while (app.pollEvent(e))
         {
             if (e.type == Event::Closed)
-                window.close();
-
-            ////move back//////
-            if (e.type == Event::KeyPressed)
-                if (e.key.code == Keyboard::BackSpace)
-                { if (position.length()>6) position.erase(position.length()-6,5); loadPosition();}
-
-            /////drag and drop///////
-            if (e.type == Event::MouseButtonPressed)
-                if (e.key.code == Mouse::Left)
-                  for(int i=0;i<32;i++)
-                  if (f[i].getGlobalBounds().contains(pos.x,pos.y))
-                      {
-                       isMove=true; n=i;
-                       dx=pos.x - f[i].getPosition().x;
-                       dy=pos.y - f[i].getPosition().y;
-                       oldPos  =  f[i].getPosition();
-                      }
-
-             if (e.type == Event::MouseButtonReleased)
-                if (e.key.code == Mouse::Left)
-                 {
-                  isMove=false;
-                  Vector2f p = f[n].getPosition() + Vector2f(size/2,size/2);
-                  newPos = Vector2f( size*int(p.x/size), size*int(p.y/size) );
-                  str = toChessNote(oldPos)+toChessNote(newPos);
-                  move(str); 
-                  if (oldPos!=newPos) position+=str+" ";
-                  f[n].setPosition(newPos);                   
-                 }                       
+                app.close();
         }
 
-       //comp move
-       if (Keyboard::isKeyPressed(Keyboard::Space))
-       {
-         str =  getNextMove(position);
-                   
-         oldPos = toCoord(str[0],str[1]);
-         newPos = toCoord(str[2],str[3]);
-         
-         for(int i=0;i<32;i++) if (f[i].getPosition()==oldPos) n=i;
-         
-         /////animation///////
-         for(int k=0;k<50;k++)
-          {
-            Vector2f p = newPos - oldPos;
-            f[n].move(p.x/50, p.y/50); 
-            window.draw(sBoard);
-            for(int i=0;i<32;i++) f[i].move(offset);
-            for(int i=0;i<32;i++) window.draw(f[i]); window.draw(f[n]);
-            for(int i=0;i<32;i++) f[i].move(-offset);
-            window.display();
-          }
+  int speed=0;
 
-        move(str);  position+=str+" ";
-        f[n].setPosition(newPos); 
-        }
+  if (Keyboard::isKeyPressed(Keyboard::Right)) playerX+=0.1;
+  if (Keyboard::isKeyPressed(Keyboard::Left)) playerX-=0.1;
+  if (Keyboard::isKeyPressed(Keyboard::Up)) speed=200;
+  if (Keyboard::isKeyPressed(Keyboard::Down)) speed=-200;
+  if (Keyboard::isKeyPressed(Keyboard::Tab)) speed*=3;
+  if (Keyboard::isKeyPressed(Keyboard::W)) H+=100;
+  if (Keyboard::isKeyPressed(Keyboard::S)) H-=100;
 
-        if (isMove) f[n].setPosition(pos.x-dx,pos.y-dy);
+  pos+=speed;
+  while (pos >= N*segL) pos-=N*segL;
+  while (pos < 0) pos += N*segL;
 
-    ////// draw  ///////
-    window.clear();
-    window.draw(sBoard);
-    for(int i=0;i<32;i++) f[i].move(offset);
-    for(int i=0;i<32;i++) window.draw(f[i]); window.draw(f[n]);
-    for(int i=0;i<32;i++) f[i].move(-offset);
-    window.display();
+  app.clear(Color(105,205,4));
+  app.draw(sBackground);
+  int startPos = pos/segL;
+  int camH = lines[startPos].y + H;
+  if (speed>0) sBackground.move(-lines[startPos].curve*2,0);
+  if (speed<0) sBackground.move( lines[startPos].curve*2,0);
+
+  int maxy = height;
+  float x=0,dx=0;
+
+  ///////draw road////////
+  for(int n = startPos; n<startPos+300; n++)  
+   {
+    Line &l = lines[n%N];
+    l.project(playerX*roadW-x, camH, startPos*segL - (n>=N?N*segL:0));
+    x+=dx;
+    dx+=l.curve;
+
+    l.clip=maxy;
+    if (l.Y>=maxy) continue;
+    maxy = l.Y;
+
+    Color grass  = (n/3)%2?Color(16,200,16):Color(0,154,0);
+    Color rumble = (n/3)%2?Color(255,255,255):Color(0,0,0);
+    Color road   = (n/3)%2?Color(107,107,107):Color(105,105,105);
+
+    Line p = lines[(n-1)%N]; //previous line
+
+    drawQuad(app, grass, 0, p.Y, width, 0, l.Y, width);
+    drawQuad(app, rumble,p.X, p.Y, p.W*1.2, l.X, l.Y, l.W*1.2);
+    drawQuad(app, road,  p.X, p.Y, p.W, l.X, l.Y, l.W);
+   }
+
+    ////////draw objects////////
+    for(int n=startPos+300; n>startPos; n--)
+      lines[n%N].drawSprite(app);
+
+    app.display();
     }
-
-    CloseConnection();
 
     return 0;
 }
